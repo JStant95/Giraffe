39793dacf837375dbf75e15c6f0a832f3e028a5d
Jed Stanton <jedstanton@hotmail.com>
2020-08-04 13:25:43 +0100

    sparql-client gem added & first test passing

--- lib/filmography.rb
+++ lib/filmography.rb
@@ -1,5 +1,19 @@
 require 'sparql/client'
 
 class Filmography
-
+  def self.show
+    query= "
+    PREFIX dbo: <http://dbpedia.org/ontology/>
+    PREFIX prop: <http://dbpedia.org/property/>
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      SELECT * WHERE {
+        ?f rdf:type dbo:Film .
+        ?f dbo:starring dbr:Macaulay_Culkin .
+      }"
+    results = []
+    sparql = SPARQL::Client.new("http://dbpedia.org/sparql")
+    sparql.query(query).each { |result| results.push(result) }
+    results
+  end
 end
--- spec/filmography_spec.rb
+++ spec/filmography_spec.rb
@@ -3,7 +3,7 @@ require 'filmography'
 describe Filmography do
   describe '.show' do
     it "Returns a list of actor filmography" do
-      expect(Filmography.show.length).to eq(11)
+      expect(Filmography.show.length).to eq(10)
     end
   end
 end
