1280c11078dc63d7b327bd72145006210889c494
Jed Stanton <jedstanton@hotmail.com>
2020-08-04 15:14:34 +0100

    Refactor - extracted create_query methd from show

--- lib/filmography.rb
+++ lib/filmography.rb
@@ -3,7 +3,17 @@ require 'sparql/client'
 class Filmography
   def self.show(actor)
     p actor
-    query= "
+    query = create_query(actor)
+    results = []
+    sparql = SPARQL::Client.new("http://dbpedia.org/sparql")
+    sparql.query(query).each { |result| results.push(result.film_title.to_s) }
+    films = { "films" => results }
+    p films
+    films
+  end
+
+  def self.create_query(actor)
+    "
     PREFIX dbo: <http://dbpedia.org/ontology/>
     PREFIX prop: <http://dbpedia.org/property/>
     PREFIX foaf: <http://xmlns.com/foaf/0.1/>
@@ -14,11 +24,5 @@ class Filmography
         ?f rdfs:label ?film_title .
         FILTER LANGMATCHES( LANG(?film_title), 'en')
       }"
-    results = []
-    sparql = SPARQL::Client.new("http://dbpedia.org/sparql")
-    sparql.query(query).each { |result| results.push(result.film_title.to_s) }
-    films = { "films" => results }
-    p films
-    films
   end
 end
