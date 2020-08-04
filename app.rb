99e5ede8ee2dc50650f707048ecbbbb9df5c539b
Jed Stanton <jedstanton@hotmail.com>
2020-08-04 18:42:08 +0100

    Typo fixed

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra/base'
 require './lib/filmography.rb'
+require './lib/cast.rb'
 
 class FilmInfo < Sinatra::Base
   get '/' do
-    Filmography.show(params[:actor]).to_json
+    if params[:actor]
+      Filmography.show(params[:actor]).to_json
+    elsif params[:film]
+      Cast.show(params[:film]).to_json
+    else
+      "Please input an actor or film as a parameter"
+    end
   end
 
   run! if app_file == $0
